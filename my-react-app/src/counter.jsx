f57870721591106a2b6b6044540be010fae60517
balakoyyamani <balakoyyamani@gmail.com>
2026-07-29 17:39:05 +0530

    update: reset button

--- my-react-app/src/counter.jsx
+++ my-react-app/src/counter.jsx
@@ -4,17 +4,21 @@ function Counter(){
     const[count,setCount]=useState(0)
 
     function increase(){
-        setCount(count+1);
+        setCount((prev)=>prev+1);
     }
     function decrease(){
-        setCount(count-1);
+        setCount((prev)=>prev-1);
+    }
+    function reset(){
+        setCount(0);
     }
     return(
         <div>
             <h2>Counter App⏲️</h2>
             <h1>{count}</h1>
             <button onClick={increase}>Increase</button>
-            <button onClick={decrease}>Decrease</button>
+            <button onClick={()=>setCount((prev)=>prev-1)}>Decrease</button>
+            <button onClick={()=>setCount(0)}>Reset</button>
         </div>
     );
 }
